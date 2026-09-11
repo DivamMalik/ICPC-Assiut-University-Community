eaaa1008258b991c657c732cd0cbb0919577c652
Divam Malik <divammalik01@gmail.com>
2026-09-11 11:33:19 +0530

    Update problemH.c

--- Sheet-1/problemH.c
+++ Sheet-1/problemH.c
@@ -9,3 +9,28 @@ int main(){
     printf("ceil %.0f / %.0f = %d\n", x , y , b);
     printf("round %.0f / %.0f = %d", x ,y ,c);
 }
+
+// -----------------------OR-----------------------
+
+#include <stdio.h>
+ 
+int main (){
+    float n1;
+    float n2;
+    scanf("%f %f",&n1, &n2);
+    float div = n1/n2 ;
+    int ceil = (int)div +1;
+    int floor = (int)div;
+    printf("floor %d / %d = %d\n",(int)n1,(int)n2,floor);
+    if ((int)div == div)
+    {
+        printf("ceil %d / %d = %d\n",(int)n1,(int)n2,floor);
+    }else{
+        printf("ceil %d / %d = %d\n",(int)n1,(int)n2,ceil);
+    }
+    if((div>=(int)div+0.5)){
+        printf("round %d / %d = %d",(int)n1,(int)n2,ceil);
+    }else{
+        printf("round %d / %d = %d",(int)n1,(int)n2,floor);
+    }
+}
