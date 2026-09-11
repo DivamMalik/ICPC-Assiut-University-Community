e9b3e5b2f0b0c84e1a7bb6b86f4522c3b577d926
Divam Malik <divammalik01@gmail.com>
2026-09-11 09:56:53 +0530

    Create problem5.c
    Implement logic to check conditions between two integers.

---
+++ Contest-1/problem5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+  int a,b;
+  scanf("%d %d",&a,&b);
+    if(a==0&& b==0){
+        printf("NO");
+    }
+    else if(a==b){
+        printf("YES");
+    }
+    else if(a-1==b){
+        printf("YES");
+    }
+    else if(a==b-1){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
